70b20b915c13e3f8276a436618b4d90711528938
Cristian Palao <cristianpalaocamarasa@gmail.com>
2023-07-29 00:50:11 +0200

    Initial commit!

---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function About() {
+
+    return(
+
+        <section className="bg-gray-100 py-8">
+            <div className="container mx-auto">
+                <h2 className="text-2xl font-bold mb-4">About Me</h2>
+                <p className="text-gray-700">
+                    Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum.
+                    lorem ipsum lorem ipsum lorem ipsum.
+                </p>
+            </div>
+        </section>
+
+    )
+
+}
+
+export default About;
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+    
+    return(
+
+        <header className="bg-gray-800 text-white p-4">
+            <div className="container mx-auto">
+                <h1 className="text-3xl font-bold">Cristian</h1>
+                <p className="text-lg">Full Stack MERN Web Developer</p>
+            </div>
+        </header>
+
+    );
+
+}
+
+export default Header;
